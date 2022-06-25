8078e77fb1b9906a671eaa79f402692c711b998f
Fadilah Hakim <moch.fadilahhakim12@gmail.com>
2022-06-25 11:19:58 +0700

    test

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About {
+
+    public function index($nama,$pekerjaan){
+        echo "Hallo saya $nama, saya adalah $pekerjaan";
+    }
+
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
